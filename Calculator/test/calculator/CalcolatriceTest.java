62b921dfa2bdce533a43a47aa8e045d767860493
TeodoroAdinolfi <99508762+TeodoroAdinolfi@users.noreply.github.com>
2022-10-20 18:16:43 +0200

    creati i test su tutte le operazioni del primo sprint

--- Calculator/test/calculator/CalcolatriceTest.java
+++ Calculator/test/calculator/CalcolatriceTest.java
@@ -41,7 +41,7 @@ public class CalcolatriceTest {
         //
     }
     
-        @Test
+    @Test
     public void testDifference() {
 
         assertEquals(1, a.difference(4,3),DELTA); // entrambi i numeri positivi
@@ -52,6 +52,50 @@ public class CalcolatriceTest {
         // to do -> uno degli operandi è al confine di rappresentazione
         //
     }
+    
+    @Test
+    public void testProduct() {
+
+        assertEquals(12, a.product(4,3),DELTA); // entrambi i numeri positivi
+        assertEquals(12, a.product(-4,-3),DELTA); // entrambi i numeri negativi
+        assertEquals(-12, a.product(4,-3),DELTA); // il primo maggiore in modulo
+        assertEquals(-12, a.product(3,-4),DELTA); // il secondo maggiore in modulo
+        assertEquals(0, a.product(4,0),DELTA); // uno degli operandi è 0
+        // to do -> uno degli operandi è al confine di rappresentazione
+        //
+    }
+    
+    @Test
+    public void testRatio() {
+
+        assertEquals(2, a.ratio(8,4),DELTA); // entrambi i numeri positivi
+        assertEquals(2, a.ratio(-8,-4),DELTA); // entrambi i numeri negativi
+        assertEquals(-2, a.ratio(8,-4),DELTA); // il primo maggiore in modulo
+        assertEquals(-0.5, a.ratio(4,-8),DELTA); // il secondo maggiore in modulo
+        assertEquals(0, a.ratio(0,4),DELTA); // uno degli operandi è 0
+        // to do -> uno degli operandi è al confine di rappresentazione
+        //
+    }
+    
+    @Test(expected=ZeroDivisionException.class)
+    public void testRatio2(){
+        a.ratio(3, 0);
+    }
+    
+    @Test
+    public void testParse(){
+        assertTrue(a.parse("1", "c", "2").equalsIgnoreCase("Operando non supportato"));
+        assertTrue(a.parse("1", "+", "2").equalsIgnoreCase("3.0"));
+        assertTrue(a.parse("1", "-", "2").equalsIgnoreCase("-1.0"));
+        assertTrue(a.parse("1", "*", "2").equalsIgnoreCase("2.0"));
+        assertTrue(a.parse("8", "/", "2").equalsIgnoreCase("4.0"));
+        
+        //assertEquals("3",a.parse("1", "+", "2"));
+        //assertEquals("2",a.parse("4", "-", "2"));
+    }
+    
+    
+    
     
  
     
