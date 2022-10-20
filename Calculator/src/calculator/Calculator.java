a5f8e6080cd7ff0b5a362cfa198f06402f246cfb
graziaFerrara <grazia.ferrara2000@gmail.com>
2022-10-21 00:12:53 +0200

    modifiche, alessio perdonami

--- Calculator/src/calculator/Calculator.java
+++ Calculator/src/calculator/Calculator.java
@@ -17,19 +17,22 @@ import java.util.logging.Logger;
  */
 public class Calculator {
     
-    private static final String opBinarie[]= {"tan","=","sin","cos","arctan"};
+    private static final String opUnarie[]= {"CM"};
+    private static final String opBinarie[]= {"tan","=","sin","cos","arctan","M+","M-"};
+    private static final String opTernarie[]= {"+","-","/","*"};
    
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         Calcolatrice cal = new Calcolatrice();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String val1 = "";
         String op = "";
         String val2 = "";
         String res = "";
+        int op2 = 0;
 
         System.out.print("\nCALCOLATRICE POLACCA\n");
 
@@ -38,32 +41,35 @@ public class Calculator {
                 
                 System.out.print("\nInserisci operazione: ");
                 op = br.readLine();
-                boolean op2=false;
-                for(String x : opBinarie) if(x.equalsIgnoreCase(op)) op2=true;
                 
-                if (op2){
-                    System.out.print("\nInserisci unico operando: ");
-                    val1 = br.readLine();
-                    res = cal.parse(op, val1);
-                } else {
-                    System.out.print("\nInserisci operando 1: ");
-                    val1 = br.readLine();
-                    System.out.print("\nInserisci operando 2: ");
-                    val2 = br.readLine();
-                    res = cal.parse(op, val1, val2);
+                // call function
+                op2 = initOp2(op);
+                
+                switch(op2){
+                    
+                    case 0:
+                        System.out.println("Operazione non supportata!");
+                        break;
+                    case 1:
+                        cal.parse(op);
+                        System.out.println("Ho inizializzato la memoria!");
+                        break;
+                    case 2:
+                        System.out.print("\nInserisci unico operando: ");
+                        val1 = br.readLine();
+                        res = cal.parse(op, val1);
+                        System.out.println("Risultato: "+res);
+                        break;
+                    case 3:
+                        System.out.print("\nInserisci operando 1: ");
+                        val1 = br.readLine();
+                        System.out.print("\nInserisci operando 2: ");
+                        val2 = br.readLine();
+                        res = cal.parse(op, val1, val2);
+                        System.out.println("Risultato: "+res);
+                        break;   
+           
                 }
-                System.out.println("Risultato: "+res);
-                /*
-            System.out.print("\nInserisci operazione: ");
-            op = br.readLine();
-            if(op.equalsIgnoreCase("=")) res = cal.parse(val1, op);
-            else{
-            System.out.print("\nInserisci operando 2: ");
-            val2= br.readLine();
-            res =cal.parse(val1, op, val2);
-            }
-            System.out.println("Risultato: "+res);
-                 */
 
             } catch (IOException ex) {
                 Logger.getLogger(Calculator.class.getName()).log(Level.SEVERE, null, ex);
@@ -71,7 +77,13 @@ public class Calculator {
 
         }
     }
+
+    private static int initOp2(String op) {
+        for (String i : opUnarie){ if (i.equalsIgnoreCase(op)) {return 1;} }
+        for (String i : opBinarie){ if (i.equalsIgnoreCase(op)) {return 2;} }
+        for (String i : opTernarie){ if (i.equalsIgnoreCase(op)) {return 3;} }
+        return 0; 
+    }
 }
-// System.out.println("\nOperando 1: " + val1 + "\nOperazione: "+op + "\nOperando 2: "+val2);
 
 
